403b78686c1404d6b0cfd0780a4371bfc80a8226
ardaaskinn01 <ardaaskindm@gmail.com>
2025-06-28 02:17:36 +0300

    last updates

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,23 @@
-import Flutter
 import UIKit
+import Flutter
+import OneSignal
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // OneSignal başlat
+    OneSignal.initialize("d4f432ca-d0cc-4d13-873d-b24b41de5699") // ← Buraya kendi OneSignal App ID'ni yaz
+
+    // Bildirim yetkisi iste (opsiyonel ama önerilir)
+    OneSignal.Notifications.requestPermission({ accepted in
+        print("Notification permission: \(accepted)")
+    }, fallbackToSettings: true)
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
